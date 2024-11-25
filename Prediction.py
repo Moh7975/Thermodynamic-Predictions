017c5af6ffb73888a28bf7675ce32628dca724a9
Mohammed <mka7975@nyu.edu>
2024-11-25 14:10:57 -0500

    Added base code

---
+++ Prediction.py
@@ -0,0 +1,15 @@
+import CoolProp.CoolProp as CP  # Thermodynamic Tables
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+#Creates a random data list of values
+def data_generation():
+    data=[]
+    return(data)
+
+#If data set does not exist run the generation
+data=[]
+if data==0:
+    data_generation()
